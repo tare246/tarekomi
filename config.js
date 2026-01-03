f38310bf81c7dcdebff49ebc30302b1e19666bfe
tare246 <138754606+tare246@users.noreply.github.com>
2026-01-03 18:40:44 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
-// Firebase コンソールで取得した設定を貼り付けてください。
-// 例: const firebaseConfig = { apiKey: "...", authDomain: "...", projectId: "..." };
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyBtAG9XstKNC_AZdt6OnofAKztp989mB7w",
   authDomain: "tarekomi-cc62c.firebaseapp.com",
   projectId: "tarekomi-cc62c",
@@ -8,3 +12,6 @@ export const firebaseConfig = {
   messagingSenderId: "404045241500",
   appId: "1:404045241500:web:d71597ac21959687232159"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
